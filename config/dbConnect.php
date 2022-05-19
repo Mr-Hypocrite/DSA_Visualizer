07f22218938d0649e0417d30d766075ad18a108d
Mr-Hypocrite <ym.jatin@gmail.com>
2022-05-19 09:35:43 +0530

    Change connect config

--- config/dbConnect.php
+++ config/dbConnect.php
@@ -1,21 +1,21 @@
 <?php
 
-    $dir = $_SERVER['DOCUMENT_ROOT'];
+    try {
+        $dir = $_SERVER['DOCUMENT_ROOT'];
 
-    require $dir.'/vendor/autoload.php';
+        require $dir.'/vendor/autoload.php';
 
-    $dotenv = Dotenv\Dotenv::createImmutable($dir);
-    $dotenv->load();
+        $dotenv = Dotenv\Dotenv::createImmutable($dir);
+        $dotenv->load();
 
-    $userName = $_ENV['userName'];
-    $password = $_ENV['password'];
-    $database = $_ENV['database'];
-    $port = $_ENV['port'];
-    $host = $_ENV['host'];
+        $userName = $_ENV['userName'];
+        $password = $_ENV['password'];
+        $database = $_ENV['database'];
+        $port = $_ENV['port'];
+        $host = $_ENV['host'];
 
-    $con = pg_connect("host=". $host .  " port=" . $port . " dbname=" . $database . " user=" . $userName . " password=" . $password . " sslmode=require");
-
-    if (!$con) {
-        die("Error");
+        $con = pg_connect("host=". $host .  " port=" . $port . " dbname=" . $database . " user=" . $userName . " password=" . $password . " sslmode=require");
+    } catch (e) {
+        echo "Couldn't connect to the database, Error: $e";
     }
 ?>
